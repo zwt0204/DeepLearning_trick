ca9108a8b3b5dc0a694ffee15f55bd4f0f69fb44
zhangweitao <zhang.weitao@chinaredstar.cn>
2020-01-02 13:27:08 +0800

    'x'

--- 优化算法/Nadam.py
+++ 优化算法/Nadam.py
@@ -33,10 +33,10 @@ def nadam(alpha, loops, epsilon):
     """
     count = 0
     # 随机thata向量初始的值,也就是随机起点位置
-    thata = np.random.randn(2)
+    theta = np.random.randn(2)
     k = np.random.randn(2)
     l = np.random.randn(2)
-    # 上次thata的值，初始为0向量
+    # 上次theta的值，初始为0向量
     err = np.zeros(2)
 
     while count < loops:
@@ -44,28 +44,28 @@ def nadam(alpha, loops, epsilon):
 
         for i in range(m):
             # np.dot()两个数组的点积
-            cost = (np.dot(thata, train_data[i]) - y[i]) * train_data[i]
+            cost = (np.dot(theta, train_data[i]) - y[i]) * train_data[i]
             k = beat1 * k + (1 - beat1) * cost
             l = beat2 * l + (1 - beat2) * (cost ** 2)
             kb = k / (1 - beat1 ** count)
             lb = l / (1 - beat2 ** count)
-            thata = thata - (alpha / (lb**0.5 + epsilon)) * (beat1 * kb + (1-beat1) * cost / (1-beat1**count))
+            thata = theta - (alpha / (lb**0.5 + epsilon)) * (beat1 * kb + (1-beat1) * cost / (1-beat1**count))
 
-        if np.linalg.norm(thata - err) < epsilon:  # 判断是否收敛
+        if np.linalg.norm(theta - err) < epsilon:  # 判断是否收敛
             break
         else:
-            err = thata  # 没有则将当前thata向量赋值给err,作为下次判断收敛的参数之一
-    print(u'Sgd结果:\tloop_counts: %d\tthata[%f, %f]' % (count, thata[0], thata[1]))
-    return thata
+            err = theta  # 没有则将当前thata向量赋值给err,作为下次判断收敛的参数之一
+    print(u'Sgd结果:\tloop_counts: %d\tthata[%f, %f]' % (count, theta[0], theta[1]))
+    return theta
 
 
 if __name__ == '__main__':
-    thata = nadam(alpha=0.001, loops=10000, epsilon=1e-5)
+    theta = nadam(alpha=0.001, loops=10000, epsilon=1e-5)
 
     # 将训练数据导入stats的线性回归算法，以作验证
     slope, intercept, r_value, p_value, slope_std_error = stats.linregress(x, y)
     print(u'Stats结果:\tintercept(截距):%s\tslope(斜率):%s' % (intercept, slope))
 
     plt.plot(x, y, 'k+')
-    plt.plot(x, thata[1] * x + thata[0], 'r')
+    plt.plot(x, theta[1] * x + theta[0], 'r')
     plt.show()
